cb78db1012e2bf98666bd72ce0b212b429e8c6f3
Emmanuel-Odero <emmanuelodero808@gmail.com>
2021-11-19 03:43:00 +0000

    Another chang, bug to be fixed. not complete

--- projects/terminal-butterfly/wings.cpp
+++ projects/terminal-butterfly/wings.cpp
@@ -14,6 +14,19 @@ int main()
         for(int j =i;j>=1;j--){
             cout<<"*";
         }
-        cout<<end1;
+        cout<<endl;
+    }
+    for(int i=1; i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        int count =2 * n-2 * i;
+        for(int j=1;j<=count;j++){
+            cout<<" ";
+        }
+        for(int j =i;j>=1;j--){
+            cout<<"*";
+        }
+        cout<<endl;
     }
 }
